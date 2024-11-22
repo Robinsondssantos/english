f44bb9925879def46eb2b423ec5bec760a9318f3
Robinson D. S. Santos <robinsonsantos@inobram.com.br>
2024-11-22 16:25:59 -0300

    fix load image

--- body-parts/script.js
+++ body-parts/script.js
@@ -4,271 +4,271 @@ window.onload = function () {
       sentence: "Head",
       translation: "Cabeça",
       audio: "./songs/head.mp3",
-      image: ""
+      image: "./images/head.jpg"
     },
     {
       sentence: "Scalp",
       translation: "Couro cabeludo",
       audio: "./songs/scalp.mp3",
-      image: ""
+      image: "./images/scalp.jpg"
     },
     {
       sentence: "Hair",
       translation: "Cabelo",
       audio: "./songs/hair.mp3",
-      image: ""
+      image: "./images/hair.webp"
     },
     {
       sentence: "Forehead",
       translation: "Testa",
       audio: "./songs/forehead.mp3",
-      image: ""
+      image: "./images/forehead.jpg"
     },
     {
       sentence: "Eyebrow",
       translation: "Sobrancelha",
       audio: "./songs/eyebrow.mp3",
-      image: ""
+      image: "./images/eyebrow.jpg"
     },
     {
       sentence: "Eye",
       translation: "Olho",
       audio: "./songs/eye.mp3",
-      image: ""
+      image: "./images/eye.jpg"
     },
     {
       sentence: "Eyelid",
       translation: "Pálpebra",
       audio: "./songs/eyelid.mp3",
-      image: ""
+      image: "./images/eyelid.jpg"
     },
     {
       sentence: "Eyelashes",
       translation: "Cílios",
       audio: "./songs/eyelashes.mp3",
-      image: ""
+      image: "./images/eyelashes.jpg"
     },
     {
       sentence: "Ear",
       translation: "Orelha",
       audio: "./songs/ear.mp3",
-      image: ""
+      image: "./images/ear.jpg"
     },
     {
       sentence: "Nose",
       translation: "Nariz",
       audio: "./songs/nose.mp3",
-      image: ""
+      image: "./images/nose.webp"
     },
     {
       sentence: "Mouth",
       translation: "Boca",
       audio: "./songs/mouth.mp3",
-      image: ""
+      image: "./images/mouth.jpg"
     },
     {
       sentence: "Throat",
       translation: "Garganta",
       audio: "./songs/throat.mp3",
-      image: ""
+      image: "./images/throat.jpg"
     },
     {
       sentence: "Lips",
       translation: "Lábios",
       audio: "./songs/lips.mp3",
-      image: ""
+      image: "./images/lips.jpg"
     },
     {
       sentence: "Tooth",
       translation: "Dente",
       audio: "./songs/tooth.mp3",
-      image: ""
+      image: "./images/tooth.png"
     },
     {
       sentence: "Teeth",
       translation: "Dentes",
       audio: "./songs/teeth.mp3",
-      image: ""
+      image: "./images/teeth.png"
     },
     // {
     //   sentence: "Wisdom teeth",
     //   translation: "Dentes do siso",
     //   audio: "./songs/wisdom.mp3",
-    //   image: ""
+    //   image: "./images/face.jpg"
     // },
     {
       sentence: "Tongue",
       translation: "Língua",
       audio: "./songs/tongue.mp3",
-      image: ""
+      image: "./images/tongue.jpg"
     },
     {
       sentence: "Cheek",
       translation: "Bochecha",
       audio: "./songs/cheek.mp3",
-      image: ""
+      image: "./images/cheek.jpg"
     },
     {
       sentence: "Freckles",
       translation: "Sardas",
       audio: "./songs/freckles.mp3",
-      image: ""
+      image: "./images/freckles.jpg"
     },
     {
       sentence: "Face dimple",
       translation: "Covinhas do rosto",
       audio: "./songs/face.mp3",
-      image: ""
+      image: "./images/face.jpg"
     },
     {
       sentence: "Beard",
       translation: "Barba",
       audio: "./songs/beard.mp3",
-      image: ""
+      image: "./images/beard.jpg"
     },
     {
       sentence: "Moustache",
       translation: "Bigode ",
       audio: "./songs/moustache.mp3",
-      image: ""
+      image: "./images/moustache.webp"
     },
     {
       sentence: "Chin",
       translation: "Queixo",
       audio: "./songs/chin.mp3",
-      image: ""
+      image: "./images/chin.jpg"
     },
     {
       sentence: "Jaw",
       translation: "Mandíbula",
       audio: "./songs/jaw.mp3",
-      image: ""
+      image: "./images/jaw.jpg"
     },
     {
       sentence: "Body",
       translation: "Corpo",
       audio: "./songs/body.mp3",
-      image: ""
+      image: "./images/body.jpg"
     },
     {
       sentence: "Neck",
       translation: "Pescoço",
       audio: "./songs/neck.mp3",
-      image: ""
+      image: "./images/neck.jpg"
     },   
     // {
     //   sentence: "Adam’s apple",
     //   translation: "Pomo de adão",
     //   audio: "./songs/jaw.mp3",
-    //   image: ""
+    //   image: "./images/face.jpg"
     // },   
     {
       sentence: "Nape",
       translation: "Nuca",
       audio: "./songs/nape.mp3",
-      image: ""
+      image: "./images/nape.jpeg"
     },
     {
       sentence: "Shoulder",
       translation: "Ombro",
       audio: "./songs/shoulder.mp3",
-      image: ""
+      image: "./images/shoulder.jpg"
     },
     {
       sentence: "Armpit",
       translation: "Axila",
       audio: "./songs/armpit.mp3",
-      image: ""
+      image: "./images/armpit.webp"
     },
     {
       sentence: "Chest",
       translation: "Peito",
       audio: "./songs/chest.mp3",
-      image: ""
+      image: "./images/chest.jpg"
     },
     {
       sentence: "Nipple",
       translation: "Mamilo",
       audio: "./songs/nipple.mp3",
-      image: ""
+      image: "./images/nipple.jpg"
     },
     {
       sentence: "Arm",
       translation: "Braço",
       audio: "./songs/arm.mp3",
-      image: ""
+      image: "./images/arm.webp"
     },
     {
       sentence: "Elbow",
       translation: "Cotovelo",
       audio: "./songs/elbow.mp3",
-      image: ""
+      image: "./images/elbow.png"
     },
     {
       sentence: "Forearm",
       translation: "Antebraço",
       audio: "./songs/forearm.mp3",
-      image: ""
+      image: "./images/forearm.webp"
     },
     {
       sentence: "Wrist",
       translation: "Pulso",
       audio: "./songs/wrist.mp3",
-      image: ""
+      image: "./images/wrist.jpg"
     },
     {
       sentence: "Fist",
       translation: "Punho",
       audio: "./songs/fist.mp3",
-      image: ""
+      image: "./images/fist.png"
     },
     {
       sentence: "Hand",
       translation: "Mão",
       audio: "./songs/hand.mp3",
-      image: ""
+      image: "./images/hand.jpg"
     },
     {
       sentence: "Palm",
       translation: "Palma da mão",
       audio: "./songs/palm.mp3",
-      image: ""
+      image: "./images/palm.jpg"
     },
     // {
     //   sentence: "Thumb finger",
     //   translation: "Dedo polegar",
     //   audio: "./songs/jaw.mp3",
-    //   image: ""
+    //   image: "./images/face.jpg"
     // },
     // {
     //   sentence: "Pointer finger",
     //   translation: "Dedo indicador",
     //   audio: "./songs/jaw.mp3",
-    //   image: ""
+    //   image: "./images/face.jpg"
     // },
     // {
     //   sentence: "Middle finger",
     //   translation: "Dedo médio",
     //   audio: "./songs/jaw.mp3",
-    //   image: ""
+    //   image: "./images/face.jpg"
     // },
     // {
     //   sentence: "Ring finger",
     //   translation: "Dedo anelar",
     //   audio: "./songs/jaw.mp3",
-    //   image: ""
+    //   image: "./images/face.jpg"
     // },
     // {
     //   sentence: "Pinky finger",
     //   translation: "Dedo mindinho",
     //   audio: "./songs/jaw.mp3",
-    //   image: ""
+    //   image: "./images/face.jpg"
     // },
     {
       sentence: "Knuckle",
       translation: "Junta",
       audio: "./songs/knuckle.mp3",
-      image: ""
+      image: "./images/knuckle.jpg"
     }
   ]
 
@@ -395,9 +395,11 @@ window.onload = function () {
 
     const btnCheck = document.getElementById("btn-check");
 
+    const image = document.getElementById("image");
     const audio = document.getElementById("audio");
     // audio.src = './songs/beard.mp3';
     // audio.load();
+
     const btnPlayer = document.getElementById("btn-player");
     btnPlayer.onclick = () => { audio.play(); };
 
@@ -409,6 +411,7 @@ window.onload = function () {
     let _word = getWord(dictionary, randomInteger);
     let word = _word.sentence;
     let translation = _word.translation;
+    image.src = _word.image;
     audio.src = _word.audio;
     audio.load();
     // btnPlayer.click();
@@ -438,6 +441,7 @@ window.onload = function () {
       _word = getWord(dictionary, randomInteger);
       word = _word.sentence;
       translation = _word.translation;
+      image.src = _word.image;
       audio.src = _word.audio;
       audio.load();
       audio.play();
